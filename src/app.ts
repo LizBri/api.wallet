98628f6d192fc6dee6cd3e4598d39533841c8546
Elizabeth Bringas <bringas.liza@gmail.com>
2022-10-28 13:00:28 -0300

    Comienzo del uso de APP_ENV

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.APP_ENV = process.env.APP_ENV || 'development';
+
+//Env files
+import dotenv = require('dotenv');
+
+dotenv.config({
+    path: `${__dirname}/../config/${process.env.APP_ENV}.env`
+})
+console.log(process.env.APP_FOO);
+
+
+
+//Para definir el comportamiento de la app
+import express = require('express');
+
+const app: express.Application = express();
+
+// Levantamiento en el navegador
+ app.get('/', (req, res) =>{
+    res.send('Hola Mundo');
+ })
+
+ export { app };
---
+++ src/server.ts
@@ -0,0 +1,6 @@
+//Para levantar el servidor
+import { app } from './app';
+
+app.listen(3000, () => {
+    console.log('Application is Running on port 3000');  
+});
