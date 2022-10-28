8a2a18594e13c27aac49d82bfdd484e1efbd5a94
Elizabeth Bringas <bringas.liza@gmail.com>
2022-10-28 13:13:22 -0300

    Contenedor y servicio fecha

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,6 @@ dotenv.config({
 })
 console.log(process.env.APP_FOO);
 
-
-
 //Para definir el comportamiento de la app
 import express = require('express');
 
@@ -20,5 +18,13 @@ const app: express.Application = express();
  app.get('/', (req, res) =>{
     res.send('Hola Mundo');
  })
+
+ //Contenedor
+ import {container} from './container';
+import { TestService } from './services/test.service';
+
+ const testService = container.resolve<TestService>('testService');
+ console.log(testService.get());
+ 
 
  export { app };
